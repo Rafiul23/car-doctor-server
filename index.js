146fa0eab20b8c0b0cdf508e3ccc3dd513896841
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-13 19:50:02 +0600

    api for single service data is ready

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -35,6 +35,13 @@ async function run() {
 
     app.get('/services', async(req, res)=>{
         const result = await serviceCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
         res.send(result);
     })
 
