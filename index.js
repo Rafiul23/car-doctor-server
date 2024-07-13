1fa83999dafa71cf0eb6cb23e4fddfc13db142d9
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-13 12:20:14 +0600

    added 2 middleware

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res)=>{
     res.send('Car doctor server is running')
 })
 
 app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port
-        
-    }`);
+    console.log(`Server is running on port: ${port}`);
 })
