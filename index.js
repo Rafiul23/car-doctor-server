e37c684168c99e97a9ac1ee88dc48f4c427c59c7
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-13 18:34:53 +0600

    get all services data is successful

--- index.js
+++ index.js
@@ -29,9 +29,20 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    const serviceCollection = client.db('carDoctor').collection('servicesDB');
+    const bookingCollection = client.db('carDoctor').collection('booking');
+
+    app.get('/services', async(req, res)=>{
+        const result = await serviceCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
