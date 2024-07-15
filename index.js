beb35982931509841ef65a5877c434356fe2edfe
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-15 22:00:02 +0600

    api for getting data by query parameter is successful

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlof2pa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,42 +26,56 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    
-    const serviceCollection = client.db('carDoctor').collection('servicesDB');
-    const bookingsCollection = client.db('carDoctor').collection('bookings');
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-    app.get('/services', async(req, res)=>{
-        const result = await serviceCollection.find().toArray();
-        res.send(result);
-    })
+    const serviceCollection = client.db("carDoctor").collection("servicesDB");
+    const bookingsCollection = client.db("carDoctor").collection("bookings");
 
-    app.get('/services/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await serviceCollection.findOne(query);
-        res.send(result);
-    })
+    app.get("/services", async (req, res) => {
+      const result = await serviceCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
 
     // to get selected data and speecific field:
-    app.get('/service/:id', async(req, res)=>{
+    app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const options = {
-        projection: {title: 1, price: 1, service_id: 1}
+        projection: { title: 1, price: 1, img: 1, service_id: 1 },
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
-    })
+    });
 
     // api for post a booking
-    app.post('/bookings', async(req, res)=>{
+    app.post("/bookings", async (req, res) => {
       const order = req.body;
       const result = await bookingsCollection.insertOne(order);
       res.send(result);
-    })
+    });
+
+    // api for getting specific booking data by query parameter
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
 
 
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -71,11 +83,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Car doctor server is running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('Car doctor server is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
