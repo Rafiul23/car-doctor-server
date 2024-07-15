4716e510d67ed29fd07518f1ef48f70e61a89366
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-15 16:34:17 +0600

    api for simgle data and specific field is ready

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ async function run() {
         res.send(result);
     })
 
+    // to get selected speecific data:
+    app.get('/service/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const options = {
+        projection: {title: 1, price: 1, service_id: 1}
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    })
+
 
 
   } finally {
