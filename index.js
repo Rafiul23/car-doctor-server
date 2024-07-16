6d7531a571b8c4aae3e1b232e5568f479c7cae53
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-16 12:33:11 +0600

    api for delete a booking is made ready

--- index.js
+++ index.js
@@ -73,9 +73,15 @@ async function run() {
       res.send(result);
     });
 
+    // api for delete a booking
+    app.delete('/booking/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
-    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
