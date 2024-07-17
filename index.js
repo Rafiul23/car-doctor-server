b2088fc516480fdfa16bb9035cdf7036b4f3c393
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-17 16:31:34 +0600

    jwt is send to client side

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlof2pa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,6 +37,22 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("servicesDB");
     const bookingsCollection = client.db("carDoctor").collection("bookings");
 
+
+    // auth related api
+    app.post('/jwt', async(req, res)=>{
+       const user = req.body;
+       console.log(user);
+       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '6h'});
+       res
+       .cookie('token:', token, {
+        httpOnly: true,
+        secure: false  //for production, it should be true
+       })
+       .send({success: true});
+    })
+
+
+    // service related api
     app.get("/services", async (req, res) => {
       const result = await serviceCollection.find().toArray();
       res.send(result);
@@ -74,29 +94,31 @@ async function run() {
     });
 
     // api for delete a booking
-    app.delete('/booking/:id', async(req, res)=>{
+    app.delete("/booking/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await bookingsCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // update a booking status
-    app.patch('/booking/:id', async(req, res)=>{
+    app.patch("/booking/:id", async (req, res) => {
       const id = req.params.id;
       const updateStatus = req.body;
-      const filter = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
-          status: updateStatus.status
-        }
-      }
-      const result = await bookingsCollection.updateOne(filter, updateDoc, options);
+          status: updateStatus.status,
+        },
+      };
+      const result = await bookingsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
-    })
-
-
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
